3e68e5f32a4a81b3746543cb1f20822ab90e249c
EllenSt264 <stafford.ellen@protonmail.com>
2021-06-12 01:54:23 +0100

    Add product sorting functionality for the navbar

--- products/views.py
+++ products/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from .models import Product, Category
+from django.db.models import Q
 
 
 def view_products(request):
     """ A view to display products """
 
-    return render(request, 'products/products.html')
+    products = Product.objects.all()
+    categories = None
+    query = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+    template = 'products/products.html'
+    context = {
+        'products': products,
+        'current_categories': categories,
+    }
+
+    return render(request, template, context)
